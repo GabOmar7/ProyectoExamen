7873dbb8cd1be0d796e3bd27acd8a3ca82de6f26
gabri <gabrielcch11@gmail.com>
2023-12-20 00:24:57 -0300

    primera subida

---
+++ app/src/main/java/com/stomas/proyectof/Acceder.java
@@ -0,0 +1,17 @@
+package com.stomas.proyectof;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Acceder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acceder);
+    }
+
+}
---
+++ app/src/main/java/com/stomas/proyectof/Botones.java
@@ -0,0 +1,4 @@
+package com.stomas.proyectof;
+
+public class Botones {
+}
